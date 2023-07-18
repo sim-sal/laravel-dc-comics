e5da38e0b8b91ec89440e46bc50e17242d485ba6
sim-sal <simone.saladino93@gmail.com>
2023-07-19 00:24:16 +0200

    add jumbotron and footer with scss properties

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,38 @@
+<footer>
+        {{-- parte inferiore  --}}
+        <section id="my-footer">
+            <div class="container">
+
+                <div id="left-footer">
+                    <button id="my-button">SIGN-UP NOW!</button>
+                </div>
+
+                <div id="right-footer">
+
+                    <div>
+                        <h2>FOLLOW US</h2>
+                    </div>
+
+                    <div class="icons">
+                        <div class="img-icon">
+                            <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook">
+                        </div>
+                        <div class="img-icon">
+                            <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter">
+                        </div>
+                        <div class="img-icon">
+                            <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube">
+                        </div>
+                        <div class="img-icon">
+                            <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest">
+                        </div>
+                        <div class="img-icon">
+                            <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </footer>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
 @extends("layouts.main-layout")
 
 @section("content")
+    <div class="container">
+        <h1><strong>Hello World!</strong></h1>
+    </div>
 
-<div class="container">
-    <h1>Hello World!</h1>
-</div>
+
 
 @endsection
