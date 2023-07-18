07dc8bc0851c3b05c4a1c5e4a8206c685d776130
sim-sal <simone.saladino93@gmail.com>
2023-07-18 19:52:59 +0200

    add header and scss properties

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="row justify-content-center">
+        <div class="col-5 laravel">
+            <div>
+                <h1><strong>LARAVEL</strong></h1>
+            </div>
+        </div>
+        <div class="col-2 logo">
+            <div>
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+        </div>
+        <div class="col-5 comics">
+            <div>
+                <h1><strong>COMICS</strong></h1>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -6,8 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel DC Comics</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+    {{-- font awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!-- Styles -->
     @vite('resources/js/app.js')
