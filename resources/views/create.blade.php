38d8d63e25a6a729158b3a0e5a40dc61ada9de8c
sim-sal <simone.saladino93@gmail.com>
2023-07-19 17:52:07 +0200

    insert CREATE component with scss properties

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,5 +1,52 @@
 @extends("layouts.main-layout")
 
 @section("content")
-<h1>MIO FORM</h1>
+    <div class="container text-center py-5">
+
+        <h1>CREATE NEW COMIC</h1>
+
+        <div class="form-container">
+            <div>
+                <form class="text-center py-5" action="">
+                    <div class="py-3">
+                        {{-- <label for="title"><h3><strong>Title</strong></h3></label><br> --}}
+                        <input type="text" placeholder="Insert TITLE" name="title">
+                    </div>
+
+                    <div class="py-3">
+                        {{-- <label for="description"><h3><strong>Description</strong></h3></label><br> --}}
+                        <input type="text" placeholder="Insert DESCRIPTION" name="description">
+                    </div>
+
+                    <div class="py-3">
+                        {{-- <label for="thumb"><h3><strong>Thumb</strong></h3></label><br> --}}
+                        <input type="text" placeholder="Insert THUMB URL" name="thumb">
+                    </div>
+
+                    <div class="py-3">
+                        {{-- <label for="price"><h3><strong>Price</strong></h3></label><br> --}}
+                        <input type="text" placeholder="Insert PRICE" name="price">
+                    </div>
+
+                    <div class="py-3">
+                        {{-- <label for="series"><h3><strong>Series</strong></h3></label><br> --}}
+                        <input type="text" placeholder="Insert SERIES" name="series">
+                    </div>
+
+                    <div class="py-3">
+                        {{-- <label for="sale_date"><h3><strong>Sale date</strong></h3></label><br> --}}
+                        <input type="text" placeholder="Insert SALE DATE" name="sale_date">
+                    </div>
+
+                    <div class="py-3">
+                        {{-- <label for="type"><h3><strong>Type</strong></h3></label><br> --}}
+                        <input type="text" placeholder="Insert type" name="type">
+                    </div>
+
+                    <input type="submit" value="CREATE" class="mt-3 my-submit">
+                </form>
+            </div>
+        </div>
+
+    </div>
 @endsection
