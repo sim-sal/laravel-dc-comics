9fce13e1bf64b3ae4a52fb6e45e9462d695d9eb6
sim-sal <simone.saladino93@gmail.com>
2023-07-19 09:32:10 +0200

    added container in header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,29 +1,32 @@
 <header>
-    <div class="row justify-content-center">
-        <div class="col-5 laravel">
-            <div>
-                <a class="text-decoration-none" href="{{ route("index")}}">
-                    <h1><strong>LARAVEL</strong></h1>
-                </a>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-5 laravel">
+                <div>
+                    <a class="text-decoration-none" href="{{ route("index")}}">
+                        <h1><strong>LARAVEL</strong></h1>
+                    </a>
 
+                </div>
             </div>
-        </div>
-        <div class="col-2 logo">
-            <div>
-                <a href="{{ route("index")}}">
-                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
-                </a>
+            <div class="col-2 logo">
+                <div>
+                    <a href="{{ route("index")}}">
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                    </a>
+                </div>
             </div>
-        </div>
-        <div class="col-5 comics">
-            <div>
-                <a class="text-decoration-none" href="{{ route("index")}}">
-                    <h1><strong>COMICS</strong></h1>
-                </a>
+            <div class="col-5 comics">
+                <div>
+                    <a class="text-decoration-none" href="{{ route("index")}}">
+                        <h1><strong>COMICS</strong></h1>
+                    </a>
 
+                </div>
             </div>
         </div>
     </div>
+
     <div class="jumbotron">
     </div>
 </header>
